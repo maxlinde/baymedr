ba103eeaa2ec6196e6a7a7ea138da0821f3235e8
maxlinde <maximilian.linde.92@gmail.com>
2019-03-06 23:40:39 +0100

    Create meaningless object documentation
    This is just a temporary documentation. This was used test some documentation properties.

--- R/integrand_t.R
+++ R/integrand_t.R
@@ -1,3 +1,19 @@
+#' Bla
+#'
+#' @param delta A number
+#' @param t A number
+#' @param n A number
+#' @param nu A number
+#' @param mu_delta A number
+#' @param gamma A number
+#' @param kappa A number
+#'
+#' @return A number
+#' @export
+#' @import stats
+#'
+#' @examples
+#' integrand_t(1, 2, 3, 4, 5, 6, 7)
 integrand_t <- function(delta,
                         t,
                         n,
@@ -5,8 +21,10 @@ integrand_t <- function(delta,
                         mu_delta,
                         gamma,
                         kappa) {
-  dt(x = t,
-     df = nu,
-     ncp = sqrt(n) * delta) * 1 / gamma * dt(x = (delta - mu_delta) / gamma,
-                                             df = kappa)
+  dt(
+    x = t,
+    df = nu,
+    ncp = sqrt(n) * delta) * 1 / gamma * dt(
+      x = (delta - mu_delta) / gamma,
+      df = kappa)
 }
