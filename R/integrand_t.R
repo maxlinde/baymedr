4e1fc871d0bfad327d828578c8dac8140e94e8de
maxlinde <maximilian.linde.92@gmail.com>
2019-03-06 22:20:21 +0100

    Draft a first outline of integrand_t function
    The code of this function is adapted from the Open Science Framework page of Gronau, Ly, & Wagenmakers (2017).

---
+++ R/integrand_t.R
@@ -0,0 +1,12 @@
+integrand_t <- function(delta,
+                        t,
+                        n,
+                        nu,
+                        mu_delta,
+                        gamma,
+                        kappa) {
+  dt(x = t,
+     df = nu,
+     ncp = sqrt(n) * delta) * 1 / gamma * dt(x = (delta - mu_delta) / gamma,
+                                             df = kappa)
+}
