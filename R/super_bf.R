a4de3204ba16d0171face51eb4667af3d61f233a
maxlinde <maximilian.linde.92@gmail.com>
2019-03-09 00:55:19 +0100

    Add function arguments and object documentation

--- R/super_bf.R
+++ R/super_bf.R
@@ -5,32 +5,51 @@
 #' The Bayes factor resulting from \code{super_bf} tests the null hypothesis
 #' that the experimental group (e.g., a new medication) is not better than the
 #' control group (e.g., a placebo or existing medication). In other words, the
-#' null hypothesis states that the true population effect size is exactly zero.
+#' null hypothesis of \code{super_bf} states that the true population effect
+#' size is exactly zero, whereas the alternative hypothesis of \code{super_bf}
+#' states that the true population effect size is larger than zero (i.e.,
+#' \eqn{\mu_control < \mu_experimental}).
 #'
+#' @param x A vector of numeric observations for the control group.
+#' @param y A vector of numeric observations for the experimental group.
+#' @param formula A formula specifying the desired model.
+#' @param data A \code{data.frame} containing all the data (used in combination
+#' with \code{formula}).
+#' @param prior_scale A scalar, specifying the scale of the prior distribution
+#' (see Details).
+#' @param ind_samples A logical value, indicating whether the groups are
+#' independent (TRUE) or dependent (FALSE).
 #'
-#' @param formula Bla
-#' @param x Bla
-#' @param y Bla
-#' @param data Bla
+#' @return Two Bayes factors are obtained from \code{super_bf}. The first one
+#' corresponds to a one-sided alternative hypothesis (i.e.,
+#' \eqn{\mu_control < \mu_experimental}); the second one corresponds to a
+#' two-sided alternative hypothesis. Importantly, both Bayes factors refer to
+#' the evidence in favour of the alternative hypothesis.
 #'
-#' @return Bla
 #' @export
-#' @import rlang tibble
+#' @import rlang stats tibble
 #'
 #' @references
+#' Gronau, Q. F., Ly, A., & Wagenmakers, E.-J. (2018). Informed bayesian
+#' t-tests. Manuscript submitted for publication.
+#'
+#' Rouder, J. N., Speckman, P. L., Sun, D., & Morey, R. D. (2009). Bayesian t
+#' tests for accepting and rejecting the null hypothesis. \emph{Psychonomic
+#' Bulletin & Review}, \emph{16}(2), 225-237.
+#'
 #' van Ravenzwaaij, D., Monden, R., Tendeiro, J. N., & Ioannidis, J. P. A.
 #' (2019). Bayes factors for superiority, non-inferiority, and equivalence
 #' designs. Manuscript submitted for publication.
 #'
-#' Gronau, Q. F., Ly, A., & Wagenmakers, E.-J. (2018). Informed bayesian
-#' t-tests. Manuscript submitted for publication.
-#'
 #' @examples
-#' Bla
-super_bf <- function(formula = NULL,
-                     x = NULL,
+#' # super_bf using x and y:
+#' super_bf(x = rnorm(100, 10, 15), y = rnorm(130, 13, 10))
+super_bf <- function(x = NULL,
                      y = NULL,
-                     data = NULL) {
+                     formula = NULL,
+                     data = NULL,
+                     prior_scale = 1 / sqrt(2),
+                     ind_samples = TRUE) {
   if (!is.null(formula) && !is.null(x)) {
     abort("You must define only one of 'formula' or 'x'.")
   }
@@ -41,7 +60,7 @@ super_bf <- function(formula = NULL,
     if (inherits(x = data,
                  what = c("tbl_df", "tbl", "data.frame"))) {
       data <- as_tibble(data)
-      warn("'data' is converted to tibble.")
+      #warn("'data' is converted to tibble.")
     }
   }
   if (!is.null(formula) && is.null(data)) {
@@ -76,13 +95,13 @@ super_bf <- function(formula = NULL,
   res = bf10_t(t = t_stat,
                n1 = n_x,
                n2 = n_y,
-               ind_samples = TRUE,
+               ind_samples = ind_samples,
                prior_loc = 0,
-               prior_scale = 1 / sqrt(2),
+               prior_scale = prior_scale,
                prior_df = 1)
   bf_sup1 = res[[3]]
   bf_sup2 = res[[1]]
   names(bf_sup1) = "BFsup1"
   names(bf_sup2) = "BFsup2"
-  return(c(bf_sup1, bf_sup2))
+  c(bf_sup1, bf_sup2)
 }
