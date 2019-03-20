9b375f62b5766167b8428a7b4725af026424eb4b
maxlinde <maximilian.linde.92@gmail.com>
2019-03-20 16:42:06 +0100

    Change function drastically
    The changes of the super_bf function include:
    -removing formula and data arguments
    -removing the possibility to specify ind_samples
    -incorporating functionality to not only work with raw data but also summary statistics
    -changing condition handling

--- R/super_bf.R
+++ R/super_bf.R
@@ -51,62 +51,77 @@
 #' super_bf(x = rnorm(100, 10, 15), y = rnorm(130, 13, 10))
 super_bf <- function(x = NULL,
                      y = NULL,
-                     formula = NULL,
-                     data = NULL,
-                     prior_scale = 1 / sqrt(2),
-                     ind_samples = TRUE) {
-  if (!is.null(formula) && !is.null(x)) {
-    abort("You must define only one of 'formula' or 'x'.")
+                     n_x = NULL,
+                     n_y = NULL,
+                     mean_x = NULL,
+                     mean_y = NULL,
+                     sd_x = NULL,
+                     sd_y = NULL,
+                     ci_margin = NULL,
+                     prior_scale = 1 / sqrt(2)) {
+  if (any(!is.null(x),
+          !is.null(y)) && any(!is.null(n_x),
+                              !is.null(n_y),
+                              !is.null(mean_x),
+                              !is.null(mean_y),
+                              !is.null(sd_x),
+                              !is.null(sd_y),
+                              !is.null(ci_margin))) {
+    abort("Only 'x', and 'y' OR 'n_x', 'n_y', 'mean_x', 'mean_y', 'sd_x', and
+          'sd_y' (or 'ci_margin' instead of 'sd_x' and 'sd_y') must be
+          defined.")
   }
-  if (xor(is.null(x), is.null(y))) {
+  if (xor(!is.null(x),
+          !is.null(y))) {
     abort("Both 'x' and 'y' must be defined.")
   }
-  if (!is.null(x)) {
-    if (any(is.na(x)) || any(!is.finite(x))) {
-      abort("'x' must not contain any missing or infinite values.")
+  if (any(!is.null(n_x),
+          !is.null(n_y),
+          !is.null(mean_x),
+          !is.null(mean_y))) {
+    if (any(is.null(n_x),
+            is.null(n_y),
+            is.null(mean_x),
+            is.null(mean_y)) &&
+        ((is.null(sd_x) || is.null(sd_y)) & is.null(ci_margin))) {
+      abort("All 'n_x', 'n_y', 'mean_x', 'mean_y', 'sd_x', and 'sd_y' (or
+            'ci_margin' instead of 'sd_x' and 'sd_y') must be defined.")
+    }
+    if (!xor(!is.null(sd_x) && !is.null(sd_y),
+             !is.null(ci_margin))) {
+      abort("Only 'sd_x' and 'sd_y' OR 'ci_margin' must be defined.")
     }
   }
-  if (!is.null(y)) {
-    if (any(is.na(y)) || any(!is.finite(y))) {
-      abort("'y' must not contain any missing or infinite values.")
+  if (!is.null(x) && !is.null(y)) {
+    if (any(is.na(x)) || any(is.na(y))) {
+      abort("'x' and 'y' must not contain missing values.")
     }
-  }
-  if (isFALSE(ind_samples) && (length(x) != length(y))) {
-    abort("If 'ind_samples' is FALSE, 'x' and 'y' must have the same length.")
-  }
-  if (!is.numeric(x) || !is.numeric(y)) {
-    abort("'x' and 'y' must be numeric.")
-  }
-  if (!is.null(data)) {
-    if (inherits(x = data,
-                 what = c("tbl_df", "tbl", "data.frame"))) {
-      data <- as_tibble(data)
-      message("'data' is converted to tibble.")
-    } else {
-      abort("'data' must be a data.frame or a tibble.")
+    if (any(is.infinite(x)) || any(is.infinite(y))) {
+      abort("'x' and 'y' must not contain infinite values.")
     }
+    if (!is.numeric(x) || !is.numeric(y)) {
+      abort("'x' and 'y' must be numeric vectors.")
+    }
+    n_x <- length(x)
+    n_y <- length(y)
+    mean_x <- mean(x)
+    mean_y <- mean(y)
+    sd_x <- sd(x)
+    sd_y <- sd(y)
   }
-  if (!is.null(formula) && is.null(data)) {
-    abort("'data' must be defined when 'formula' is used.")
+  if (!is.null(sd_x) && !is.null(sd_y)) {
+    sd_pooled <- sqrt(((n_x - 1) * sd_x ^ 2 + (n_y - 1) * sd_y ^ 2) /
+                        (n_x + n_y - 2))
+    se <- sd_pooled * sqrt(1 / n_x + 1 / n_y)
+  } else {
+    se <- ci_margin / qt(p = 0.975,
+                         df = n_x + n_y - 2)
   }
-
-
-
-
-  n_x <- length(x)
-  n_y <- length(y)
-  mean_x <- mean(x)
-  mean_y <- mean(y)
-  sd_x <- sd(x)
-  sd_y <- sd(y)
-  sd_pooled <- sqrt(((n_x - 1) * sd_x ^ 2 + (n_y - 1) * sd_y ^ 2) /
-                      (n_x + n_y - 2))
-  se <- sd_pooled * sqrt(1 / n_x + 1 / n_y)
   t_stat <- (mean_x - mean_y) / se
   res = bf10_t(t = t_stat,
                n1 = n_x,
                n2 = n_y,
-               ind_samples = ind_samples,
+               ind_samples = TRUE,
                prior_loc = 0,
                prior_scale = prior_scale,
                prior_df = 1)
