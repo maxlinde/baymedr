188b53aa5d1f0bafdc22dd912d8eb11b0e2573d8
maxlinde <maximilian.linde.92@gmail.com>
2019-04-05 14:26:25 +0200

    Create object documentation

--- R/get_bf.R
+++ R/get_bf.R
@@ -1,3 +1,33 @@
+#' Extract Bayes factor from S4 object
+#'
+#' This function extracts the Bayes factor from S4 objects.
+#'
+#' @param object An S4 object of class 'baymedrSuperiority',
+#' 'baymedrEquivalence', or 'baymedrNonInferiority'.
+#'
+#' @return A numeric scalar of length 1, providing the Bayes factor from an S4
+#' object.
+#' @export
+#'
+#' @examples
+#' # Extract Bayes factor from super_bf using raw data:
+#' mod_super <- super_bf(x = rnorm(100, 10, 15),
+#'                       y = rnorm(130, 13, 10))
+#'
+#' get_bf(object = mod_super)
+#'
+#' # Extract Bayes factor from equiv_bf using raw data:
+#' mod_equiv <- equiv_bf(x = rnorm(100, 10, 15),
+#'                       y = rnorm(130, 13, 10))
+#'
+#' get_bf(object = mod_equiv)
+#'
+#' # Extract Bayes factor from infer_bf using raw data:
+#' mod_infer <- infer_bf(x = rnorm(100, 10, 15),
+#'                       y = rnorm(130, 13, 10),
+#'                       ni_margin = -1)
+#'
+#' get_bf(object = mod_infer)
 get_bf <- function(object) {
   object@bf
 }
