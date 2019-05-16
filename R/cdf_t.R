0fc12cd4512954375066b27cd0595743a14f4711
maxlinde <maximilian.linde.92@gmail.com>
2019-05-16 12:36:21 +0200

    Fix problem of incorrect numeric integration

--- R/cdf_t.R
+++ R/cdf_t.R
@@ -1,4 +1,4 @@
-#' @import stats
+#' @import rlang stats stringr
 cdf_t <- function(x,
                   t,
                   n1,
@@ -8,15 +8,22 @@ cdf_t <- function(x,
                   prior_scale,
                   prior_df,
                   rel_tol = .Machine$double.eps^0.25) {
-  integrate(posterior_t,
-            lower = -Inf,
-            upper = x,
-            t = t,
-            n1 = n1,
-            n2 = n2,
-            ind_samples = ind_samples,
-            prior_loc = prior_loc,
-            prior_scale = prior_scale,
-            prior_df = prior_df,
-            rel.tol = rel_tol)$value
+  out <- integrate(posterior_t,
+                   lower = -Inf,
+                   upper = x,
+                   t = t,
+                   n1 = n1,
+                   n2 = n2,
+                   ind_samples = ind_samples,
+                   prior_loc = prior_loc,
+                   prior_scale = prior_scale,
+                   prior_df = prior_df,
+                   rel.tol = rel_tol)$value
+  if (out > 1 & out < 1.001) {
+    out <- 1
+    warn(str_c(
+      "Numerical integration yields a CDF value slightly larger than 1. ",
+      "The CDF value has been replaced by 1."
+    ))
+  }
 }
