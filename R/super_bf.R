4b9bafdf86e0e90e2890f4cb92a6bb730f8c4ea1
maxlinde <maximilian.linde.92@gmail.com>
2019-07-07 20:25:36 +0200

    Incorporate major updates: -Delete old version of argument 'alternative' -Implementation of 'direction' and 'alternative' as arguments -Add quality checks for new arguments -Align documentation with new arguments

--- R/super_bf.R
+++ R/super_bf.R
@@ -4,24 +4,38 @@
 #'
 #' The Bayes factor resulting from \code{\link{super_bf}} tests the null
 #' hypothesis that the experimental group (e.g., a new medication) is equal to
-#' the control group (e.g., a placebo or existing medication). In other words,
-#' the null hypothesis of \code{\link{super_bf}} states that the true population
-#' effect size is exactly zero.
+#' the control group (e.g., a placebo or an already existing medication). The
+#' formulation of the alternative hypothesis depends on whether a one- or
+#' two-sided test shall be conducted and on whether the low or high values
+#' correspond to superiority. As the name 'superiority' implies, the test is
+#' typically understood as a one-tailed test. In practice, however, a two-tailed
+#' test is often employed, followed by subsequent confirmation that the results
+#' are in the expected direction. To cover both research practices, the user has
+#' the possibility to specify which of these two alternatives should be employed
+#' through the argument \code{alternative}. Furthermore, since sometimes high
+#' scores on the dependent variable are considered superior (e.g., amount of
+#' social interactions) and sometimes rather the low scores (e.g., severity of
+#' symptoms), the user can specify the direction of superiority with the
+#' argument \code{direction}. For the case where higher values on the dependent
+#' variable indicate superiority, the user should specify 'high' (the default)
+#' for the argument \code{direction}; if lower values on the dependent variable
+#' indicate superiority, 'low' should be specified for the argument
+#' \code{direction}. Therefore, in case a two-sided test is employed, the
+#' alternative hypothesis states that the experimental group is not equal to the
+#' control group. In the case of a one-sided test and low scores corresponding
+#' to superiority, the alternative hypothesis is that the experimental group is
+#' lower than the control group. Lastly, in the case where a one-sided test is
+#' employed and high scores indicate superiority, the alternative hypothesis
+#' states that the experimental group is higher than the control group.
 #'
-#' As the name 'superiority' implies, the test is typically understood as a
-#' one-tailed test. In practice, however, a two-tailed test is often employed,
-#' followed by subsequent inspection of the descriptive statistics to confirm
-#' that results are in the expected direction. To cover both research practices,
-#' the user has the possibility to specify which of these two alternatives
-#' should be employed through the argument \code{alternative}. Furthermore,
-#' since sometimes high scores on the dependent variable are considered superior
-#' (e.g., amount of social interactions) and sometimes rather the low scores
-#' (e.g., severity of symptoms), the user can specify the direction of
-#' superiority with the argument \code{alternative}. For the case where higher
-#' values on the dependent variable indicate superiority, the user should
-#' specify 'greater' (the default) for the argument \code{alternative}; if lower
-#' values on the dependent variable indicate superiority, 'less' should be
-#' specified for the argument \code{alternative}.
+#' Since the main goal of \code{\link{super_bf}} is to establish superiority,
+#' the resulting Bayes factor quantifies evidence in favour of the alternative
+#' hypothesis (i.e., \eqn{BF10}). However, evidence for the null hypothesis can
+#' easily be calculated by taking the reciprocal of the original Bayes factor
+#' (i.e., \eqn{BF01 = 1 / BF10}). Quantification of evidence in favour of the
+#' null hypothesis is logically sound and legitimate within the Bayesian
+#' framework but not in the traditional frequentist framework (see e.g., van
+#' Ravenzwaaij et al., 2019).
 #'
 #' Importantly, \code{\link{super_bf}} can be utilized to calculate a Bayes
 #' factor based on raw data (i.e., if arguments \code{x} and \code{y} are
@@ -29,20 +43,26 @@
 #' \code{mean_x}, and \code{mean_y} are defined). In the latter case, the user
 #' has the freedom to supply values either for the arguments \code{sd_x} and
 #' \code{sd_y} \strong{OR} \code{ci_margin}. The choice should depend on the
-#' information that is available to the user.
+#' information that is available to the user. Arguments with 'x' as a name or
+#' suffix correspond to the control group, whereas arguments with 'y' as a name
+#' or suffix correspond to the experimental group (i.e., the group for which we
+#' seek to establish superiority).
 #'
-#' The argument \code{prior_scale} specifies the width of the prior distribution
-#' on effect size. This prior follows a Cauchy distribution with one degree of
-#' freedom. Visually, it resembles a normal distribution, although with much
-#' heavier tails (see e.g., Rouder et al., 2009). Mathematically, a Cauchy
-#' distribution with one degree of freedom is equivalent to a normal
-#' distribution with a mean of zero and a variance that follows an inverse
-#' chi-square distribution with one degree of freedom, for which the variance is
-#' integrated out (Liang et al., 2008). \code{prior_scale} corresponds to half
-#' of the interquartile range of the Cauchy prior. In general, the larger the
-#' value for \code{prior_scale}, the broader the Cauchy prior distribution, and
-#' the higher the relative support for the null hypothesis, reflected in the
-#' resulting Bayes factor.
+#' For the calculation of the Bayes factor, we chose a Cauchy prior density for
+#' the effect size under the alternative hypothesis. The shape of the Cauchy
+#' distribution can be manipulated with its location and scale parameters. The
+#' standard Cauchy distribution, with a location parameter of 0 and a scale
+#' parameter of 1, resembles a standard Normal distribution, except that the
+#' Cauchy distribution has less mass at the centre but heavier tails (see e.g.,
+#' Rouder et al., 2009, for a visualisation). Mathematically, the standard
+#' Cauchy distribution is equivalent to a Normal distribution with a mean of 0
+#' and a variance that follows and inverse chi-square distribution with one
+#' degree of freedom, for which the variance is integrated out (Liang et al.,
+#' 2008). The argument \code{prior_scale} specifies the width of the Cauchy
+#' prior, which corresponds to half of the interquartile range. Thus, by
+#' adjusting the Cauchy prior scale with \code{prior_scale}, we can emphasise
+#' different ranges of effect sizes that might be expected. The default prior
+#' scale is set to \eqn{1 / \sqrt{2}}.
 #'
 #' \code{\link{super_bf}} creates an S4 object of class 'baymedrSuperiority',
 #' which has multiple slots/entries (e.g., type of data, prior scale, Bayes
@@ -72,12 +92,17 @@
 #'   experimental groups. Only \code{sd_x} and \code{sd_y} \strong{OR}
 #'   \code{ci_margin} should be defined (see Details).
 #' @param prior_scale A numeric vector of length one, specifying the scale of
-#'   the prior distribution (see Details). The default value is \eqn{1 /
-#'   \sqrt{2}}.
-#' @param alternative A character vector of length one, specifying the direction
-#'   of the alternative hypothesis. 'greater' (the default) corresponds to mu_y
-#'   > mu_x, two.sided' reflects mu_y == mu_x, and 'less' refers to mu_y < mu_x
-#'   (see Details).
+#'   the Cauchy prior distribution for the effect size under the alternative
+#'   hypothesis (see Details). The default value is \eqn{1 / \sqrt{2}}.
+#' @param direction A character vector of length one, specifying the direction
+#'   of superior scores. 'low' indicates that low scores on the measure of
+#'   interest correspond to a superior outcome and 'high' (the default)
+#'   indicates that high scores on the measure of interest correspond to a
+#'   superior outcome (see Details).
+#' @param alternative A character vector of length one, specifying whether a
+#'   one-sided or two-sided test shall be conducted. 'one.sided' (the default)
+#'   corresponds to a one-sided test and 'two.sided' corresponds to a two-sided
+#'   test (see Details).
 #'
 #' @return An S4 object of class 'baymedrSuperiority' is returned. Contained are
 #'   a description of the model and the resulting Bayes factor: \itemize{ \item
@@ -85,10 +110,10 @@
 #'   \itemize{ \item h0: The null hypothesis \item h1: The alternative
 #'   hypothesis} \item data: A description of the data \itemize{ \item type: The
 #'   type of data ('raw' when arguments \code{x} and \code{y} are used or
-#'   'summary' when arguments \code{n_x}, \code{n_y}, \code{mean_x},
+#'   summary' when arguments \code{n_x}, \code{n_y}, \code{mean_x},
 #'   \code{mean_y}, \code{sd_x}, and \code{sd_y} (or \code{ci_margin} instead of
 #'   \code{sd_x} and \code{sd_y}) are used) \item ...: values for the arguments
-#'   used, depending on 'raw' or 'summary'} \item prior_scale: The width of the
+#'   used, depending on 'raw' or 'summary'} \item prior_scale: The scale of the
 #'   Cauchy prior distribution \item bf: The resulting Bayes factor } A summary
 #'   of the model is shown by printing the object.
 #'
@@ -96,7 +121,7 @@
 #' @import rlang stats stringr
 #'
 #' @references Gronau, Q. F., Ly, A., & Wagenmakers, E.-J. (2019). Informed
-#'   bayesian t-tests. \emph{The American Statistician}, 1-13.
+#'   bayesian t-tests. \emph{The American Statistician}.
 #'
 #'   Liang, F., Paulo, R., Molina, G., Clyde, M. A., & Berger, J. O. (2008).
 #'   Mixtures of g priors for bayesian variable selection. \emph{Journal of the
@@ -133,7 +158,8 @@
 #'                               mean_x = 68.1,
 #'                               mean_y = 63.6,
 #'                               ci_margin = (15.5 - (-6.5)) / 2,
-#'                               alternative = "less")
+#'                               direction = "low",
+#'                               alternative = "one.sided")
 #'
 #' # Extract Bayes factor from model.
 #' get_bf(super_sum_one_ex1)
@@ -168,7 +194,8 @@
 #'                               mean_x = 47.6,
 #'                               mean_y = 61.3,
 #'                               ci_margin = (24.4 - 2.9) / 2,
-#'                               alternative = "less")
+#'                               direction = "low",
+#'                               alternative = "one.sided")
 #'
 #' # Extract Bayes factor from model.
 #' get_bf(super_sum_one_ex2)
@@ -199,7 +226,8 @@ super_bf <- function(x = NULL,
                      sd_y = NULL,
                      ci_margin = NULL,
                      prior_scale = 1 / sqrt(2),
-                     alternative = "greater") {
+                     direction = "high",
+                     alternative = "one.sided") {
   if (any(!is.null(x),
           !is.null(y)) && any(!is.null(n_x),
                               !is.null(n_y),
@@ -273,6 +301,9 @@ super_bf <- function(x = NULL,
   if (!is.numeric(prior_scale) || length(prior_scale) > 1) {
     abort("'prior_scale' must be a single numeric value.")
   }
+  if (!is.character(direction) || length(direction) > 1) {
+    abort("'direction' must be a single character value.")
+  }
   if (!is.character(alternative) || length(alternative) > 1) {
     abort("'alternative' must be a single character value.")
   }
@@ -293,19 +324,24 @@ super_bf <- function(x = NULL,
                 prior_scale = prior_scale,
                 prior_df = 1)
   if (str_detect(alternative,
-                 "greater")) {
-    bf <- res[[2]]
-    h1 <- "mu_y > mu_x"
+                 "one.sided")) {
+    if (str_detect(direction,
+                   "low")) {
+      bf <- res[[3]]
+      h1 <- "mu_y < mu_x"
+    } else if (str_detect(direction,
+                          "high")) {
+      bf <- res[[2]]
+      h1 <- "mu_y > mu_x"
+    } else {
+      abort("'direction' must be one of 'low' or 'high'.")
+    }
   } else if (str_detect(alternative,
                         "two.sided")) {
     bf <- res[[1]]
     h1 <- "mu_y != mu_x"
-  } else if (str_detect(alternative,
-                        "less")) {
-    bf <- res[[3]]
-    h1 <- "mu_y < mu_x"
   } else {
-    abort("'alternative' must be one of 'greater', 'two.sided', or 'less'.")
+    abort("'alternative' must be one of 'one.sided' or 'two.sided'.")
   }
   test <- "Superiority analysis"
   h0 <- "mu_y == mu_x"
