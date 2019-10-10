03783bfdab1743b974296f1ccb6f9bed8dfb4ed7
maxlinde <maximilian.linde.92@gmail.com>
2019-10-10 15:37:55 +0200

    Shorten on-attach message

--- R/on_attach.R
+++ R/on_attach.R
@@ -3,7 +3,6 @@
   packageStartupMessage(
 "#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#
 Welcome to ", pkgname, ": Computation of Bayes Factors for Common Biomedical Designs.
-Please refer to the README (https://github.com/maxlinde/baymedr) for more instructions.
 In case you have questions, contact Maximilian Linde (maximilian.linde.92@gmail.com).
 #%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#")
 }
