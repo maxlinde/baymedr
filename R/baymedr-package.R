073d2968cf08af9289193c9a92543eb4d879a052
maxlinde <maximilian.linde.92@gmail.com>
2021-03-16 11:04:21 +0100

    Update package description.

--- R/baymedr-package.R
+++ R/baymedr-package.R
@@ -1,8 +1,7 @@
 #' baymedr: Computation of Bayes Factors for Common Biomedical Designs
 #'
 #' \code{baymedr} provides functions for the computation of Bayes factors for
-#' common biomedical research designs. At this point in time, functions for
-#' equivalence, non-inferiority, and superiority designs are implemented.
+#' common biomedical research designs.
 #'
 #' \tabular{ll}{ Package: \tab baymedr \cr Type: \tab Package \cr Version: \tab
 #' 0.1.0 \cr License: \tab GPL-3 \cr Date: \tab 2019-10-17 \cr}
