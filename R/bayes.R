7605213c9139975ad97253806c9c25ea2b897da8
maxlinde <maximilian.linde.92@gmail.com>
2022-06-14 18:16:32 +0200

    Put helper functions into separate file.

---
+++ R/bayes.R
@@ -0,0 +1,79 @@
+#' @import bridgesampling rstan
+likelihood <- function(time,
+                       event,
+                       group,
+                       null_value = 0,
+                       log = TRUE) {
+  surv <- time[event == 1]
+  surv_ord_uniq <- unique(sort(surv))
+  res_i <- 0
+  for (i in 1:length(surv_ord_uniq)) {
+    d_set <- which(time == surv_ord_uniq[i] & event == 1)
+    d <- length(d_set)
+    tmp_1 <- sum(group[d_set] * null_value)
+    res_j <- 0
+    for (j in 1:d) {
+      tmp_2a <- sum(exp(group[time >= surv_ord_uniq[i]] * null_value))
+      tmp_2b <- (j - 1) / d
+      tmp_2c <- sum(exp(group[d_set] * null_value))
+      res_j <- res_j + log(tmp_2a - tmp_2b * tmp_2c)
+    }
+    res_i <- res_i + tmp_1 - res_j
+  }
+  ifelse(test = log,
+         yes = res_i,
+         no = exp(res_i))
+}
+
+posterior <- function(time,
+                      event,
+                      group,
+                      null_value = 0,
+                      alternative = "two.sided",
+                      direction = NULL,
+                      prior_mean = 0,
+                      prior_sd = 1,
+                      chains = 4,
+                      iter = 21000,
+                      warmup = 1000,
+                      thin = 1,
+                      cores = 1) {
+  stan_data <- list(n_time = length(time),
+                    time = time,
+                    event = event,
+                    group = group,
+                    n_surv = length(unique(time[event == 1])),
+                    surv_ord_uniq = unique(sort(time[event == 1])),
+                    prior_mean = prior_mean,
+                    prior_sd = prior_sd,
+                    null_hypothesis = null_value,
+                    alternative = ifelse(test = alternative == "two.sided",
+                                         yes = 0,
+                                         no = 1),
+                    direction = ifelse(test = is.null(direction),
+                                       yes = 0,
+                                       no = ifelse(test = direction == "low",
+                                                   yes = -1,
+                                                   no = 1)))
+  sampling(object = stanmodels$coxph_bf,
+           data = stan_data,
+           chains = chains,
+           iter = iter,
+           warmup = warmup,
+           thin = thin,
+           cores = cores,
+           refresh = max(iter / 10, 1),
+           control = list(adapt_delta = 0.99))
+}
+
+marginal_likelihood <- function(object,
+                                cores = 1,
+                                log = TRUE) {
+  log_marg_lik <- bridge_sampler(samples = object,
+                                 cores = cores,
+                                 use_neff = FALSE,
+                                 silent = TRUE)$logml
+  ifelse(test = log,
+         yes = log_marg_lik,
+         no = exp(log_marg_lik))
+}
--- R/coxph_bf.R
+++ R/coxph_bf.R
@@ -1,5 +1,5 @@
 #' @export
-#' @import bridgesampling
+#' @import bridgesampling rstan
 coxph_bf <- function(time,
                      event,
                      group,
@@ -15,56 +15,30 @@ coxph_bf <- function(time,
                      thin = 1,
                      cores = 1,
                      save_samples = FALSE) {
-  stan_data <- list(n_time = length(time),
-                    time = time,
+  log_lik <- likelihood(time = time,
+                        event = event,
+                        group = group,
+                        null_value = null_value,
+                        log = TRUE)
+  post <- posterior(time = time,
                     event = event,
                     group = group,
-                    n_surv = length(unique(time[event == 1])),
-                    surv_ord_uniq = unique(sort(time[event == 1])),
+                    null_value = null_value,
+                    alternative = alternative,
+                    direction = direction,
                     prior_mean = prior_mean,
                     prior_sd = prior_sd,
-                    null_hypothesis = null_value,
-                    alternative = ifelse(test = alternative == "two.sided",
-                                         yes = 0,
-                                         no = 1),
-                    direction = ifelse(test = is.null(direction),
-                                       yes = 0,
-                                       no = ifelse(test = direction == "low",
-                                                   yes = -1,
-                                                   no = 1)))
-  stan_fit <- sampling(object = stanmodels$coxph_bf,
-                       data = stan_data,
-                       chains = chains,
-                       iter = iter,
-                       warmup = warmup,
-                       thin = thin,
-                       cores = cores,
-                       refresh = 0,
-                       control = list(adapt_delta = 0.99))
-  log_marg_lik <- bridge_sampler(samples = stan_fit,
-                                 cores = cores,
-                                 use_neff = FALSE,
-                                 silent = TRUE)$logml
-  surv <- time[event == 1]
-  surv_ord_uniq <- unique(sort(surv))
-  res_i <- 0
-  for (i in 1:length(surv_ord_uniq)) {
-    d_set <- which(time == surv_ord_uniq[i] & event == 1)
-    d <- length(d_set)
-    tmp_1 <- sum(group[d_set] * null_value)
-    res_j <- 0
-    for (j in 1:d) {
-      tmp_2a <- sum(exp(group[time >= surv_ord_uniq[i]] * null_value))
-      tmp_2b <- (j - 1) / d
-      tmp_2c <- sum(exp(group[d_set] * null_value))
-      res_j <- res_j + log(tmp_2a - tmp_2b * tmp_2c)
-    }
-    res_i <- res_i + tmp_1 - res_j
-  }
-  log_lik <- res_i
+                    chains = chains,
+                    iter = iter,
+                    warmup = warmup,
+                    thin = thin,
+                    cores = cores)
+  log_marg_lik <- marginal_likelihood(object = post,
+                                      cores = cores,
+                                      log = TRUE)
   log_bf10 <- log_marg_lik - log_lik
   if (save_samples) {
-    samples <- stan_fit
+    samples <- post
   } else {
     samples <- NA
   }
