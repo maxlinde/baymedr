fdfc659db8e0b39e0177e8275c8e9a4142e66762
maxlinde <maximilian.linde.92@gmail.com>
2022-06-17 10:03:40 +0200

    Update tests for get_bf().

--- tests/testthat/test-get_bf.R
+++ tests/testthat/test-get_bf.R
@@ -13,6 +13,23 @@ mod_infer <- infer_bf(x = con,
                       y = exp,
                       ni_margin = 0.5)
 
+time <- survival::aml$time
+event <- survival::aml$status
+group <- survival::aml$x
+group <- ifelse(test = group == "Maintained",
+                yes = 1,
+                no = 0)
+
+mod_coxph <- coxph_bf(time = time,
+                      event = event,
+                      group = group,
+                      save_samples = FALSE)
+
+mod_coxph_samples <- coxph_bf(time = time,
+                              event = event,
+                              group = group,
+                              save_samples = TRUE)
+
 test_that("get_bf extracts numeric Bayes factor from S4 object", {
   expect_true(
     is.numeric(get_bf(mod_super))
@@ -23,6 +40,12 @@ test_that("get_bf extracts numeric Bayes factor from S4 object", {
   expect_true(
     is.numeric(get_bf(mod_infer))
   )
+  expect_true(
+    is.numeric(get_bf(mod_coxph))
+  )
+  expect_true(
+    is.numeric(get_bf(mod_coxph_samples))
+  )
 })
 
 test_that("get_bf gives correct error messages", {
@@ -31,8 +54,9 @@ test_that("get_bf gives correct error messages", {
                   y = exp)),
     str_c(
       "Bayes factors can only be extracted from S4 objects of classes ",
-      "'baymedrEquivalence', 'baymedrNonInferiority', and ",
-      "'baymedrSuperiority'."
+      "'baymedrEquivalence', 'baymedrNonInferiority', ",
+      "'baymedrSuperiority', 'baymedrCoxProportionalHazards', and ",
+      "'baymedrCoxProportionalHazardsSamples'."
     ),
     fixed = TRUE
   )
