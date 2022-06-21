d7ba471feb1fd362b2a44ee9ff0997744f5dc921
maxlinde <maximilian.linde.92@gmail.com>
2022-06-21 20:25:38 +0200

    Remove welcome message.

--- R/zzz.R
+++
@@ -1,8 +0,0 @@
-.onAttach <- function(libname,
-                      pkgname) {
-  packageStartupMessage(
-"#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#
-Welcome to ", pkgname, ": Computation of Bayes Factors for Common Biomedical Designs.
-In case you have questions, contact Maximilian Linde (maximilian.linde.92@gmail.com).
-#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#%#")
-}
